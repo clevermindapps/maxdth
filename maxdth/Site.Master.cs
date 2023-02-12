df48d76908c2b968b53fbbff5047b39ddc288c4b
Binnu1112 <susmithavari1112@outlook.com>
2023-02-12 17:25:49 +0530

    login button updated

--- maxdth/Site.Master.cs
+++ maxdth/Site.Master.cs
@@ -56,7 +56,7 @@ namespace maxdth
             Session["fullname"] = "";
             Session["usertype"] = "";
             Session["userphoto"] = "";
-            Response.Redirect("Default.aspx");
+            Response.Redirect("https://www.maxdth.com");
         }
     }
 }
