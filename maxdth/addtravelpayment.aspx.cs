3ac9796c373c2a2371ee8aac760916aa828ae262
Anil Kumar <anilkumar.uppalapti@gmail.com>
2023-02-25 13:24:35 +0530

    addtravelpayment,modifyattence

--- maxdth/addtravelpayment.aspx.cs
+++ maxdth/addtravelpayment.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +11,9 @@ namespace maxdth
 {
     public partial class addtravelpayment : System.Web.UI.Page
     {
+        string strconn = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        DateTime addeddatetime = DateTime.Now;
+        DateTime modifieddatetime = DateTime.Now;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +21,36 @@ namespace maxdth
 
         protected void addtravelpaymentbutton_Click(object sender, EventArgs e)
         {
+            try
+            {
+                SqlConnection con = new SqlConnection(strconn);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO travelpayment_tbl(isactive,empname,amount,paiddate,remarks,addedby,addeddatetime,modifiedby,modifieddatetime) values(@isactive,@empname,@amount,@paiddate,@remarks,@addedby,@addeddatetime,@modifiedby,@modifieddatetime)", con);
 
+                cmd.Parameters.AddWithValue("@isactive", "1");
+                cmd.Parameters.AddWithValue("@empname", employeename.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@amount", travelamount.Text.Trim());
+                cmd.Parameters.AddWithValue("@paiddate", paymentdate.Text.Trim());
+                cmd.Parameters.AddWithValue("@remarks", remarks.Text.Trim());
+                cmd.Parameters.AddWithValue("@addedby", Session["fullname"].ToString());
+                cmd.Parameters.AddWithValue("@addeddatetime", addeddatetime.ToString());
+                cmd.Parameters.AddWithValue("@modifiedby", Session["fullname"].ToString());
+                cmd.Parameters.AddWithValue("@modifieddatetime", modifieddatetime.ToString());
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<Script>alert('Travel Payment Added Successfully')</script>");
+
+                employeename.ClearSelection();
+                travelamount.Text = "";
+                paymentdate.Text = "";
+                remarks.Text = "";
+                
+            }
+            catch (Exception)
+            {
+                Response.Write("<Script>alert('Travel Payment Not Added')</script>");
+            }
         }
     }
 }
--- maxdth/adminempattendancemodify.aspx.cs
+++ maxdth/adminempattendancemodify.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +11,8 @@ namespace maxdth
 {
     public partial class adminempattendancemodify : System.Web.UI.Page
     {
+        string strconn = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        DateTime modifieddatetime = DateTime.Now;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,12 +20,76 @@ namespace maxdth
 
         protected void attendanceidbutton_Click(object sender, EventArgs e)
         {
+            try
+            {
+                SqlConnection conn = new SqlConnection(strconn);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM attendance_tbl where attendanceid='" + attendanceid.Text + "'", conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        isactive.SelectedValue = dr[0].ToString();
+                        monthcode.Text = dr[2].ToString();
+                        empfullname.Text = dr[3].ToString();
+                        dateandtime.Text = dr[4].ToString();
+                        fulldayorhalfday.Text = dr[5].ToString();
+                        isonleave.Text = dr[6].ToString();
+                        remarks.Text = dr[7].ToString();
+                    }
+                }
+                else
+                {
+                    Response.Write("<Script>alert('Please Enter Correct Attendence ID ')</script>");
+
+                    attendanceid.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+                Response.Write("<Script>alert('Please Enter Correct Attendence ID ')</script>");
+
+                attendanceid.Text = "";
+            }
 
         }
 
         protected void modifyattendancebutton_Click(object sender, EventArgs e)
         {
+            try
+            {
+                SqlConnection con = new SqlConnection(strconn);
+                con.Open();
+                SqlCommand com = new SqlCommand("UPDATE attendance_tbl SET isactive=@isactive,monthcode=@monthcode,userfullname=@userfullname,datetime=@datetime,fulldayorhalfday=@fulldayorhalfday,isonleave=@isonleave,remarks=@remarks,modifiedby=@modifiedby,modifieddatetime=@modifieddatetime WHERE attendanceid='" + attendanceid.Text.Trim() + "'", con);
 
+                com.Parameters.AddWithValue("@isactive", isactive.SelectedItem.Value);
+                com.Parameters.AddWithValue("@monthcode", monthcode.Text.Trim());
+                com.Parameters.AddWithValue("@userfullname", empfullname.Text.Trim());
+                com.Parameters.AddWithValue("@datetime", dateandtime.Text.Trim());
+                com.Parameters.AddWithValue("@fulldayorhalfday", fulldayorhalfday.Text.Trim());
+                com.Parameters.AddWithValue("@isonleave", isonleave.Text.Trim());
+                com.Parameters.AddWithValue("@remarks", remarks.Text.Trim());
+                com.Parameters.AddWithValue("@modifiedby", Session["fullname"].ToString());
+                com.Parameters.AddWithValue("@modifieddatetime", modifieddatetime.ToString());
+
+                com.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<Script>alert('Attendance Modified Successfully')</script>");
+
+                isactive.ClearSelection();
+                attendanceid.Text = "";
+                monthcode.Text = "";
+                empfullname.Text = "";
+                dateandtime.Text = "";
+                fulldayorhalfday.Text = "";
+                isonleave.Text = "";
+                remarks.Text = "";
+            }
+            catch (Exception)
+            {
+                Response.Write("<Script>alert('Attendance Not Modified')</script>");
+            }
         }
     }
 }
